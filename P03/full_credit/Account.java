87485e5c51d2b36323fdf8766e58cffe9fc286fd
Hadi <hkudoda@protonmail.com>
2025-02-03 20:11:40 -0600

    Working code and got rid of comments.

--- P03/full_credit/Account.java
+++ P03/full_credit/Account.java
@@ -1,18 +1,13 @@
 public class Account
 {
-    //-name : String
     private String name;
 
-    //-id : int
     private int id;
 
-    //-nextID : int
     private static int nextID = 1;
 
-    //-status : AccountStatus
     private AccountStatus status;
 
-    //+Account(name : String)
     public Account(String name)
     {
         if (name == null || name.length() == 0)
@@ -24,25 +19,21 @@ public class Account
         this.status = AccountStatus.Normal;
     }
 
-    //+setStatus(status : AccountStatus)
     public void setStatus(AccountStatus status)
     {
         this.status = status;
     }
 
-    //+isMuted() : Boolean
     public Boolean isMuted()
     {
         return status != AccountStatus.Normal;
     }
 
-    //+isBlocked() : Boolean
     public Boolean isBlocked()
     {
         return status == AccountStatus.Blocked;
     }
 
-    //+toString() : String <<overide>>
     public String toString()
     {
         if (status == AccountStatus.Normal) 
--- P03/full_credit/Message.java
+++ P03/full_credit/Message.java
@@ -4,22 +4,16 @@ import java.util.Date;
 
 public class Message
 {
-    //-from : Account
     private Account from;
     
-    //-date : Date
     private Date date;
     
-    //-repliedTo : Message
     private Message repliedTo;
     
-    //-replies : Message[] <<ArrayList>>
     private ArrayList<Message> replies;
 
-    //-body : String
     private String body;
 
-    //+Message(from : Account, repliedTo : Message, body : String)
     public Message(Account from, Message repliedTo, String body)
     {
         this.from = from;
@@ -32,13 +26,11 @@ public class Message
         }
     }
 
-    //+getRepliedTo() : Message
     public Message getRepliedTo()
     {
         return repliedTo;
     }
     
-    //+getReply(index : int) : Message
     public Message getReply(int index)
     {
         if (index < 0 || index >= replies.size()) 
@@ -48,46 +40,36 @@ public class Message
         return replies.get(index);
     }
 
-    //+toString() : String <<overide>>
     @Override
     public String toString()
     {
         StringBuilder sb = new StringBuilder();
 
-        //System.out.println("Date: " + date);
-        //System.out.println("From: " + from.toString());
         sb.append("Date: ").append(date).append("\n");
         sb.append("From: ").append(from).append("\n");
 
 
         if (repliedTo != null) 
         {
-            //System.out.println("In reply to: " + repliedTo.from.toString());
             sb.append("In reply to: ").append(repliedTo.from).append("\n");
-            //System.out.println(repliedTo);
         }
-   //     System.out.println(repliedTo);
 
         if (!replies.isEmpty()) 
         {
-            //System.out.println("Replies: ");
             sb.append("Replies: ");
 
             for (int i = 0; i < replies.size(); i++) 
             {
-                //System.out.println(replies.get(i).from.toString());
                 sb.append(replies.get(i).from.toString());
                 if (i < replies.size() - 1) 
                 {
-                    //System.out.println(", ");
-                    sb.append("\n");
+                    sb.append(", ");
                 }
             }
-            //System.out.println("\n");
+
             sb.append("\n");
         }
 
-        //System.out.println("\n" + body);
         sb.append("\n").append(body);
 
         //return repliedTo.toString();
@@ -95,9 +77,8 @@ public class Message
 
     }
 
-    //-addReply(message : Message)
     private void addReply(Message message)
     {
-        repliedTo.addReply(message);
+        replies.add(message);
     }
 }
