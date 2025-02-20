687fe02526fddd4dbf44c0838909001d8f161c62
Hadi <hkudoda@protonmail.com>
2025-02-20 14:07:18 -0600

    abuta outline

---
+++ P05/full_credit/abuta.java
@@ -0,0 +1,45 @@
+public class abuta
+{
+    private Account[] accounts;
+    private Group[] groups;
+    private Message message;
+    private Menu menu;
+    private String output;
+    private boolean running;
+
+    public Abuta()
+    {
+
+    }
+
+    public void mdi()
+    {
+
+    }
+
+    private void endApp()
+    {
+
+    }
+
+    private void showRepliedTo()
+    {
+
+    }
+
+    private void showReply()
+    {
+
+    }
+
+    private void reply()
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        
+    }
+
+}
---
+++ P05/full_credit/message/Group.java
@@ -0,0 +1,44 @@
+package message;
+
+
+public class Group
+{
+    private String name;
+    private boolean active;
+
+    public Group(String name)
+    {
+        if (name == null || name.isEmpty()) 
+        {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+        this.active = true;
+    }
+
+    public boolean isActive()
+    {
+        return active;
+    }
+
+    public void disable()
+    {
+        active = false;
+    }
+
+    public void enable()
+    {
+        active = true;
+    }
+
+    @Override
+    public String toString()
+    {
+        String result = name;
+        if (!active) 
+        {
+            result += " inactive";
+        }
+        return result;
+    }
+}
