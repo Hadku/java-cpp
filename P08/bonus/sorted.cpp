58853bd64f66b8c034a3c60561246a6000a7b8cf
Hadi Kudoda <hkudoda@protonmail.com>
2025-03-28 18:26:23 -0500

    Working code for bonus.

--- P08/bonus/sorted.cpp
+++ P08/bonus/sorted.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
+#include <vector>
+#include <string>
+#include <algorithm>
 
 int main(int argc, char* argv[]) 
 {
-    std::string = 
+    std::vector<std::string> words;
+    for (int i = 1; i < argc; ++i) 
+    {
+        words.push_back(std::string(argv[i]));
+    }
+
+
+
+    std::sort(words.begin(), words.end());
+
+
+    for ( int i = 0; i < words.size(); ++i) 
+    {
+        std::cout << words[i] << std::endl;
+    }
+
 }
